2613aad53e42debc24a7f2042d161e50f291cf01
Ildar Samit <ildar.samit@gmail.com>
2015-09-29 11:25:34 -0400

    Add versions for react and browserify dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ildar:react-select',
-  version: '0.6.11',
+  version: '0.6.11_1',
   summary: 'React-Select packaged for Meteor',
   git: 'https://github.com/ildarsamit/meteor-react-select',
   documentation: 'README.md'
@@ -9,8 +9,8 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.1');
   api.use('ecmascript');
-  api.use('react');
-  api.use('cosmos:browserify');
+  api.use('react@0.1.13');
+  api.use('cosmos:browserify@0.5.1');
   api.addFiles('package.browserify.js');
   api.addFiles('package.browserify.options.json');
   api.addFiles('default.css', 'client');
