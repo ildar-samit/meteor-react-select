adb82671c25bf199afe8088642a88edc78ec7d3c
Ildar Samit <ildar.samit@gmail.com>
2015-09-29 14:09:17 -0400

    Explicitly specify that files should be loaded on the client only

--- package.js
+++ package.js
@@ -10,9 +10,9 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.0.1');
   api.use('ecmascript');
   api.use('react@0.1.13');
-  api.use('cosmos:browserify@0.7.0');
-  api.addFiles('package.browserify.js');
-  api.addFiles('package.browserify.options.json');
+  api.use('cosmos:browserify@0.7.0', 'client');
+  api.addFiles('package.browserify.js', 'client');
+  api.addFiles('package.browserify.options.json', 'client');
   api.addFiles('default.css', 'client');
   api.export('Select');
 });
